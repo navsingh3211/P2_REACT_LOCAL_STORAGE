4d661c6fe361216d1334d8a7ac202f1d794bd683
navsingh3211 <navsingh3211@gmail.com>
2024-04-19 16:45:03 +0530

    1st pr

---
+++ react-local-storage/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-dark text-white text-center p-4 mt-3">
+        <h2>Design And Developed By : nks</h2>
+        <h6>All Rights Reserved &copy; nks</h6>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
